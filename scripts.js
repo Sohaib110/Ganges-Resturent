696abf4acb90a5b84296ed6c3f0cdf99e227adae
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-03-18 21:45:09 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -325,7 +325,7 @@ function askForEmail(providedName) {
  * If yes, skip the review flow. If no, proceed to ask "Google or Facebook".
  */
 function checkExistingReward(providedEmail) {
-  let projectName = "bengal";
+  let projectName = "ganges";
   let normalizedEmail = providedEmail.trim().toLowerCase();
 
   // Store the email in localStorage for later use
